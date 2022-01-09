25b90abb7258c0e323d9a66a92e9ec7c9b173825
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2022-01-09 11:55:03 +0000

    Adding Pages & Routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Explore from './pages/Explore'
+import Offers from './pages/Offers'
+import Profile from './pages/Profile'
+import Signin from './pages/Signin'
+import Signup from './pages/Signup'
+import ForgotPassword from './pages/ForgotPassword'
 function App() {
   return (
-    <div >
-     <h1>
-     house-marketplace-react-project
-     </h1>
-    </div>
-  );
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Explore />} />
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+        </Routes>
+      </Router>
+      {/* Navbar */}
+    </>
+  )
 }
 
-export default App;
+export default App
