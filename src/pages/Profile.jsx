c39c1346cc9edbbd5846f684fc6937fe4d5c52af
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2022-01-13 15:03:12 +0000

    User Sign in

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,14 @@
-function Profile() {
-  return (
-    <div>
-      <h1>Profile</h1>
-    </div>
-  )
-}
+import { useEffect, useState } from 'react'
+import { getAuth } from 'firebase/auth'
 
+function Profile() {
+  const [user, setUser] = useState(null)
+
+  const auth = getAuth()
+  useEffect(() => {
+    setUser(auth.currentUser)
+  }, [])
+
+  return user ? <h2>{user.displayName}</h2> : 'Not logged in'
+}
 export default Profile
