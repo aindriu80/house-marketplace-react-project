9aee861930ad840e71336e032a73df638d5a5fe5
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2022-01-20 15:07:14 +0000

    Creating the Listing Page

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ import ForgotPassword from './pages/ForgotPassword'
 import Category from './pages/Category'
 import Navbar from './Navbar'
 import PrivateRoute from './components/PrivateRoute'
+import CreateListing from './pages/CreateListing'
+
 function App() {
   return (
     <>
@@ -24,6 +26,7 @@ function App() {
           <Route path="/sign-in" element={<Signin />} />
           <Route path="/sign-up" element={<Signup />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/create-listing" element={<CreateListing />} />
         </Routes>
         <Navbar />
       </Router>
