735a06f85452dead68159404706c77a5a1cf908a
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2022-01-28 13:07:30 +0000

    Added Lat Long manually, adding Listings Page Slider

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -1,16 +1,20 @@
 import { useState, useEffect } from 'react'
 import { Link, useNavigate, useParams } from 'react-router-dom'
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
+import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from 'swiper'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import 'swiper/swiper-bundle.css'
 import { getDoc, doc } from 'firebase/firestore'
 import { getAuth } from 'firebase/auth'
 import { db } from '../firebase.config'
 import Spinner from '../components/Spinner'
 import shareIcon from '../assets/svg/shareIcon.svg'
+SwiperCore.use([Navigation, Pagination, Scrollbar, A11y])
 
 function Listing() {
   const [listing, setListing] = useState(null)
   const [loading, setLoading] = useState(true)
-  const [shareLinkCopied, setShareLinkCopied] = useState(null)
+  const [shareLinkCopied, setShareLinkCopied] = useState(false)
 
   const navigate = useNavigate()
   const params = useParams()
@@ -36,7 +40,18 @@ function Listing() {
 
   return (
     <main>
-      {/* Slider     */}
+      <Swiper slidesPerView={1} pagination={{ clickable: true }}>
+        {listing.imgUrls.map((url, index) => (
+          <SwiperSlide key={index}>
+            <div
+              style={{
+                background: `url(${listing.imgUrls[index]}) center no-repeat`,
+                backgroundSize: 'cover',
+              }}
+              className="swiperSlideDiv"></div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
 
       <div
         className="shareIconDiv"
@@ -90,7 +105,7 @@ function Listing() {
 
         <div className="leafletContainer">
           <MapContainer
-            style={{ height: '100%', widht: '100%' }}
+            style={{ height: '100%', width: '100%' }}
             center={[listing.geolocation.lat, listing.geolocation.lng]}
             zoom={13}
             scrollWheelZoom={false}>
